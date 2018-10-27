2b7ebc2cd9a48ec31f5ca38fcde6ed2c793dddeb
AlexanderBS <alex.sad@bk.ru>
2018-10-27 19:22:08 +0800

    [ex2-70] Разработать простой компонент «Каталог товаров» #add creating a physical page with a menu and a call to the component #remove left_menu in directory ex2/feedback

---
+++ ex2/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Форма обратной связи",
+		"/ex2/feedback/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"simplecomp", 
+		"/ex2/simplecomp/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>

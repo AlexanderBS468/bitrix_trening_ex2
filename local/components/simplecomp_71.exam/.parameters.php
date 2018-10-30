a772701e5a072459adbf45a44c9c50c96520b51b
AlexanderBS <alex.sad@bk.ru>
2018-10-30 11:13:07 +0800

    [ex2-70] Разработать простой компонент «Каталог товаров» #add lang files, params && template

---
+++ local/components/simplecomp_71.exam/.parameters.php
@@ -0,0 +1,26 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentParameters = array(
+
+	"PARAMETERS" => array(
+		"IBLOCKS_CATALOG_ID" => array(
+			"PARENT" => "BASE",
+			"NAME" => GetMessage("IBLOCKS_CATALOG_ID"),
+		),
+		"IBLOCKS_CLASSIFIER_ID" => array(
+			"PARENT" => "BASE",
+			"NAME" => GetMessage("IBLOCKS_CLASSIFIER_ID"),
+		),
+		"TEMPLATE_DETAIL" => array(
+			"PARENT" => "BASE",
+			"NAME" => GetMessage("TEMPLATE_DETAIL"),
+		),
+		"CODE_PROPERTY_PRODUCT" => array(
+			"PARENT" => "BASE",
+			"NAME" => GetMessage("CODE_PROPERTY_PRODUCT"),
+		),
+		"CACHE_TIME"  =>  Array("DEFAULT"=>36000000),
+	),
+);
+?>

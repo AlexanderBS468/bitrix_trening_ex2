a23a20656f052bc2a37c8fb4c25dfbe33436a6ab
AlexanderBS <alex.sad@bk.ru>
2018-11-02 13:45:54 +0800

    [ex2-97] Разработать простой компонент «Новости по интересам» lang, params, description файлы.:

---
+++ local/components/simplecomp_97.exam/.description.php
@@ -0,0 +1,12 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => GetMessage("NAME"),
+	"DESCRIPTION" => GetMessage("DESCRIPTION"),
+	"SORT" => 10,
+	"PATH" => array(
+		"ID" => GetMessage("SECTION_NAME"),
+	)
+);
+?>
---
+++ local/components/simplecomp_97.exam/lang/ru/class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlexanderBS
+ * Date: 30.10.2018
+ * Time: 13:30
+ */
+$MESS ['MSG_COUNTER_TITLE_EX2_97'] = "Новостей [#COUNT#]";
+$MESS ['EX2_97_SIMPLECOMP_EXAM_CLASS__ERROR_PARAMS_EMPTY'] = 'Заполнены не все обязательные параметры';
+$MESS ['EX2_97_SIMPLECOMP_EXAM_CLASS__ERROR_ACCESS_DENY'] = 'Авторизуйтесь для доступа к контенту';
+$MESS ['EX2_97_SIMPLECOMP_EXAM_CLASS__ERROR_USER_NOT_FOUND'] = 'Пользователь не найден';
+$MESS ['IBLOCK_MODULE_NOT_INSTALLED'] = 'IBLOCK_MODULE_NOT_INSTALLED';
+
+?>

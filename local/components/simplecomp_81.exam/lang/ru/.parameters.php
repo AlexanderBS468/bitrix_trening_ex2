32c827dd417a1ae929492167f053f17a53fad77b
AlexanderBS <alex.sad@bk.ru>
2018-11-05 17:33:57 +0800

    [ex2-81] Внести доработки в созданный простой компонент «Каталог товаров» Добавлены ленговые файлы, добавлены параметры вызова компонента.

--- local/components/simplecomp_81.exam/lang/ru/.parameters.php
+++ local/components/simplecomp_81.exam/lang/ru/.parameters.php
@@ -1,3 +1,6 @@
 <?
-$MESS["MESS_PARAMETR"] = "";
+$MESS["IBLOCKS_CATALOG_ID"] = "ID инфоблока с каталогом товаров";
+$MESS["IBLOCKS_CLASSIFIER_ID"] = "ID инфоблока с классификатором";
+$MESS["TEMPLATE_DETAIL"] = "Шаблон ссылки на детальный просмотр товара";
+$MESS["CODE_PROPERTY_PRODUCT"] = "Код свойства товара, в котором хранится привязка товара к классификатору";
 ?>
--- local/components/simplecomp_81.exam/lang/ru/class.php
+++ local/components/simplecomp_81.exam/lang/ru/class.php
@@ -1,5 +1,6 @@
 <?php
 
 $MESS ['IBLOCK_MODULE_NOT_INSTALLED'] = 'Не подключен модуль IBLOCK';
-
+$MESS ['EX2_71_SIMPLECOMP_EXAM_CLASS__ERROR_PARAMS_EMPTY'] = 'Не введены обязательные параметры';
+$MESS ['EX2_71_SIMPLECOMP2_EXAM__TITLE'] = 'Разделов: #COUNT#';
 ?>

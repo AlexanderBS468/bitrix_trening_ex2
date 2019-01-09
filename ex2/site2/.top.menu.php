aa2dfc559cd6ad3499b49b4997418c0407075b38
AlexanderBS <alex.sad@bk.ru>
2019-01-09 18:51:12 +0800

    [ex2-102] Добавление верхнего меню. Добавление слогана Добавление включаемой области

---
+++ ex2/site2/.top.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Main",
+		"/ex2/site2/",
+		Array(), 
+		Array(),
+		"" 
+	),
+	Array(
+		"About",
+		"/ex2/site2/about.php",
+		Array(), 
+		Array(),
+		"" 
+	),
+	Array(
+		"HELPIK", 
+		"/helpik/",
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>

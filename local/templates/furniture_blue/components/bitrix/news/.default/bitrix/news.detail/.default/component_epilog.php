81f94f004566ab9b7805bf96bc04170a3915cd22
AlexanderBS <alex.sad@bk.ru>
2019-01-10 11:07:00 +0800

    [ex2-104] Работа логики добавление элемента.

--- local/templates/furniture_blue/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
+++ local/templates/furniture_blue/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
@@ -6,9 +6,70 @@
  * Time: 18:28
  */
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+use Bitrix\Main\Loader;
+global $USER;
 
 if ($arResult['CANONICAL']) {
 	global $APPLICATION;
 	$APPLICATION->SetPageProperty("canonical", $arResult["CANONICAL"]);
 }
+
+define("IBLOCK_ID_NEWS_COMPARE", 9);
+
+if(!$USER->IsAuthorized())
+{
+	CJSCore::Init();
+}
+
+if(isset($_GET["ID"]))
+{
+	if(Loader::includeModule("iblock"))
+	{
+		if($USER->IsAuthorized())
+		{
+			$user = $USER->GetID() . " " . $USER->GetLogin() . " " . $USER->GetFullName();
+		}
+		else
+		{
+			$user = "Не авторизован";
+		}
+
+		$el = new CIBlockElement();
+		$arFields = [
+			"IBLOCK_ID" => IBLOCK_ID_NEWS_COMPARE,
+			"NAME" => "Жалоба на новость " . $_GET["ID"],
+			"ACTIVE" => "Y",
+			"PROPERTY_VALUES" => [
+				"USER" => $user,
+				"NEWS" => $_GET["ID"]
+			],
+			"ACTIVE_FROM" => date("d.m.Y H:i:s", strtotime("now"))
+		];
+	}
+	if($PRODUCT_ID = $el->Add($arFields))
+	{
+		if($_GET["TYPE"] == "AJAX")
+		{
+			$GLOBALS["APPLICATION"]->RestartBuffer();
+			echo json_encode(["ID" => $PRODUCT_ID]);
+			die();
+		}
+		elseif($_GET['TYPE'] == 'GET')
+		{
+			if(isset($_GET["ID"])){?>
+				<script>
+					var textEl = document.getElementById('report_responce');
+					textEl.innerText = "Ваше мнение учтено, № " + <?=$PRODUCT_ID?>;
+				</script>
+			<?}else{?>
+				<script>
+					var textEl = document.getElementById('report_responce');
+					textEl.innerText = "Ошибка!";
+				</script>
+				<?
+			}
+		}
+	}
+}
+
 ?>
